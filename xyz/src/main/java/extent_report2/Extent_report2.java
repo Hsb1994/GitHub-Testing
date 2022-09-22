a246b90c8193b3443041668c852fec1ba159bd68
JollyRoger-Himanshu <adhikarihimanshu255@gmail.com>
2022-09-22 14:11:06 +0530

    Extent report updated

--- xyz/src/main/java/extent_report2/Extent_report2.java
+++ xyz/src/main/java/extent_report2/Extent_report2.java
@@ -25,10 +25,10 @@ public class Extent_report2 implements ITestListener {
 		reports.setSystemInfo("Machine", "My PC");
 		reports.setSystemInfo("OS", "Windows");
 		reports.setSystemInfo("Browser", "Chrome");
-		reports.setSystemInfo("Tool", "Eclipse");
+		reports.setSystemInfo("Tool", "Intellij");
 		
 		reporter.config().setDocumentTitle("Extent Report");
-		reporter.config().setReportName("Hemant Singh Bisht");
+		reporter.config().setReportName("Himanshu Adhikari");
 		reporter.config().setTheme(Theme.DARK);
 
 	}
